4ad0842279c90c326cb2d918949f33e680617523
adutt2020 <ajagpal.510@gmail.com>
2024-10-21 09:05:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -44,16 +44,19 @@ st.markdown("Based on the historgram above, the most expensive vehicles tend to 
 
 
 st.header("Analysis of Fastest and Slowing Selling Cars")
-fig = px.histogram(
-    Vehicles,
-    x='days_listed',
-    color='type',
-    title='Fastest and Slowest Selling Car Types',
-    labels={'days_listed': 'Days Listed', 'type': 'Car Type'},
-    color_discrete_sequence=px.colors.qualitative.Set1  # Use a predefined color sequence
-)
 
-st.plotly_chart(fig)
+show_histogram = st.checkbox('Show Histogram')
+
+if show_histogram:
+    fig = px.histogram(
+        Vehicles,
+        x='days_listed',
+        color='type',
+        title='Fastest and Slowest Selling Car Types',
+        labels={'days_listed': 'Days Listed', 'type': 'Car Type'},
+        color_discrete_sequence=px.colors.qualitative.Set1  # Use a predefined color sequence
+    )
+    st.plotly_chart(fig)
 
 st.markdown("From this histogram, we can see that buses are the slowest selling cars on this lot. We can also conclude that on average, cars are listed between 20-40 days.")
 
